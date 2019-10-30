ad72c9b92cfce16730f91b2bd97ca7273bcf43bb
youshen-chl <514292146@qq.com>
2019-10-30 22:20:48 +0800

    合并

--- js-ex19/topnav.js
+++ js-ex19/topnav.js
@@ -7,13 +7,7 @@ function myFunc(){
     }
 }
 
-<<<<<<< HEAD
 function showDetail(evt){
-    evt.currentTarget.lastElementChild.classList.toggle('show');
-    // alert(evt.currentTarget.lastElementChild.textContent)
-=======
-function showDetal(evt){
-    evt.currentTarget.children[1].classList.toggle('show')
+    evt.currentTarget.lastElementChild.classList.toggle('show')
 
->>>>>>> 37c3f08c8b9551cbfa3f1bdfb25f7d91aca351bd
 }
